1100b4dcbd9ea44b4dc5517dcde6962686b618a9
Amera <amera@gmail.com>
2024-08-04 16:47:14 +0300

    commit first react app

---
+++ first-react-app/src/App.js
@@ -0,0 +1,18 @@
+//import React from 'react';
+import Header from './Components/Header';
+
+const App=() => {
+  return (
+    <div className="container">
+      <Header/>
+    </div>
+  );
+}
+
+// class App extends React.Component{
+//   render(){
+//     return <h1> Hello from React class</h1>
+//   }
+// }
+
+export default App;
